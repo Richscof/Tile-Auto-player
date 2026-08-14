d638a5ca3edbe8f15d3104599a4b0c36a0957090
Richscof <richardscofield2009@gmail.com>
2026-08-14 03:48:22 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
