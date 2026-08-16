b9bc69e20d3daee0002334cfbe36bcb87affa301
Richscof <richardscofield2009@gmail.com>
2026-08-16 02:32:07 +0200

    Implement service worker caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,63 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "tile-auto-player-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker activated");
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function (key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(function (response) {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
+  );
 });
